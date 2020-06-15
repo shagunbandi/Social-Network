8e11c86b81ca80ceaa3a03d6854c4abea857ee79
Shagun Bandi <shagunamitbandi@gmail.com>
2020-06-15 14:26:46 +0000

    requirements server

--- Peace/settings.py
+++ Peace/settings.py
@@ -78,13 +78,21 @@ WSGI_APPLICATION = 'Peace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/home/shagunamitbandi/SocialNetwork/auth/mysql.cnf',
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
